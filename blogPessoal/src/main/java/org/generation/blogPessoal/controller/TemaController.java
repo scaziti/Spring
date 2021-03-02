accdbe216af70906f2a51ed9b7477061f6e0e10c
Raphael Scaziti <rscaziti@gmail.com>
2021-03-02 11:00:18 -0300

    feat(tema/controller): adicionado método DELETE

--- blogPessoal/src/main/java/org/generation/blogPessoal/controller/TemaController.java
+++ blogPessoal/src/main/java/org/generation/blogPessoal/controller/TemaController.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -55,5 +56,11 @@ public class TemaController
 	public ResponseEntity<TemaModel> put(@RequestBody TemaModel tema)
 	{
 		return ResponseEntity.status(HttpStatus.OK).body(this.repository.save(tema));
+	}
+	
+	@DeleteMapping ("/{id}")
+	public void delete(@PathVariable Long id)
+	{
+		this.repository.deleteById(id);
 	}
 }
