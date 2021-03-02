a9e80922d6243983438e560292249f76190af978
Raphael Scaziti <rscaziti@gmail.com>
2021-03-02 10:56:15 -0300

    feat(tema/controller): adicionado método POST

--- blogPessoal/src/main/java/org/generation/blogPessoal/controller/TemaController.java
+++ blogPessoal/src/main/java/org/generation/blogPessoal/controller/TemaController.java
@@ -6,10 +6,13 @@ import java.util.Map;
 import org.generation.blogPessoal.model.TemaModel;
 import org.generation.blogPessoal.repository.TemaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -39,5 +42,11 @@ public class TemaController
 	public ResponseEntity<List<TemaModel>> GetByDescricao(@PathVariable String descricao)
 	{
 		return ResponseEntity.ok(this.repository.findAllByDescricaoContainingIgnoreCase(descricao));
+	}
+	
+	@PostMapping
+	public ResponseEntity<TemaModel> post(@RequestBody TemaModel tema)
+	{
+		return ResponseEntity.status(HttpStatus.CREATED).body(this.repository.save(tema));
 	}
 }
